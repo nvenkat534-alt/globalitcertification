010a03ea73187bfb15acfc15c7e5ffcae5e38a2e
notvenu <venu.kasibhatla@gmail.com>
2026-09-03 22:28:02 +0530

    Add Google Tag Manager integration for enhanced tracking capabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,6 +63,15 @@ export default function RootLayout({
             gtag('config', 'G-CR0XC9N914');
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MJF8PDV8');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
@@ -85,6 +94,15 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased bg-bg-dark text-white">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MJF8PDV8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         <SmoothScroll>
           <Navbar />
           {children}
@@ -104,6 +122,7 @@ export default function RootLayout({
             </span>
           </a>
         </SmoothScroll>
+        
       </body>
     </html>
   );
